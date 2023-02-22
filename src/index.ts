49fd7be98ec512b4c3dc1ea75b31001233bcb8be
Zahar Samohval <zaxar137samohval44@gmail.com>
2023-02-23 01:48:04 +0300

    add graphql schema and fix tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,66 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import {ApolloServer} from '@apollo/server';
+import {startStandaloneServer} from '@apollo/server/standalone';
+import resolvers from "./resolvers";
+import { readFileSync } from 'fs';
 
 dotenv.config()
-const app:express.Application  = express()
+const app: express.Application = express()
 
+
+///COMMIT and push!!!!!!!!!!!!!such user has exist already
+///COMMIT and push!!!!!!!!!!!!!
+///COMMIT and push!!!!!!!!!!!!!
+///COMMIT and push!!!!!!!!!!!!!
 
 app.use(express.json())
 
 app.use(cors())
 
 
+interface MyContext {
+    models: {
+        USER: string
+    }
+}
 
-const port = process.env.PORT || "8080"
+const typeDefs = readFileSync('src/schema/schema.graphql', { encoding: 'utf-8' });
 
-app.listen(port,() =>{
-    console.log(`Server started on port: ${port}`)
-})
+const server = new ApolloServer({typeDefs, resolvers})
 
 
+const portUrl = process.env.PORT || "8080"
+
+async function start() {
+
+    if (process.env.MONGODB_URI) {
+        mongoose.connect(
+            process.env.MONGODB_URI
+        )
+            .then(() => {
+                console.log("connect with DB")
+
+            })
+            .catch((e) => {
+                console.log("don't connect with DB")
+                console.log(e)
+
+            })
+    }
+
+    const {url} = await startStandaloneServer(server, {
+        context: async ({req, res}) => {
+
+
+
+            return { }
+        },
+        listen: {port: Number(portUrl)},
+    });
+    console.log(`🚀  Server ready at: ${url}`);
+}
+
+start()
